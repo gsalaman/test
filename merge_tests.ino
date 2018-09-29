54c162f7c2524a20566e8e457a9c0bad722eb92b
gsalaman <43499190+gsalaman@users.noreply.github.com>
2018-09-29 07:52:29 -0600

    Create merge_tests.ino

---
+++ merge_tests.ino
@@ -0,0 +1,26 @@
+// This file is going to help me play with merges in github and arduino.
+
+// Master branch is going to use ver_led, dev branch is going to remove it.  When I merge
+// I'm gonna want to keep it.
+#include "ver_led.h"
+
+setup
+{
+  Serial.begin(9600);
+  
+  ver_led_setup(2);
+}
+
+loop
+{
+  static int i=0;
+  char my_string[40];
+  
+  sprintf(my_string, "Number = %d", i);
+  
+  Serial.println(my_string);
+  i++;
+  
+  ver_led_run();
+  delay(300);
+}
